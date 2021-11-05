7606a269b5c4ad393a15e9ddc36f39acb183b5ff
nrlzh <zharkynbeknurila@gmai.com>
2021-11-05 19:53:28 +0600

    practice 4: kafka fixed!

--- src/main/java/com/example/onepractice1/kafka/KafkaProducer.java
+++ src/main/java/com/example/onepractice1/kafka/KafkaProducer.java
@@ -3,7 +3,6 @@ package com.example.onepractice1.kafka;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
